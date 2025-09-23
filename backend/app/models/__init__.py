8aceb7d3da3bcf015900baeb06aee007497e8f08
peter munyambu <petermunyambu1000@gmail.com>
2025-09-23 20:36:51 +0300

    refactor: remove unused model imports for cleaner code

--- backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from app import db  # import the SQLAlchemy instance
-
-# Import models so Alembic can detect them
-from .user import User
-from .categories import Category
-from .listings import Listing
-from .favorite import Favorite
-
-__all__ = ["User", "Category", "Listing", "Favorite"]
