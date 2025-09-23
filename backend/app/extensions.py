bcb3ad9e34de31f345a11f9d71ce977ca52ef66c
Vivian Gichure <viviangichure@gmail.com>
2025-09-23 07:13:37 +0300

    Add initial extensions and compiled auth routes

---
+++ backend/app/extensions.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+ma = Marshmallow()
+migrate = Migrate()
