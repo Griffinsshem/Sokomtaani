978b3740a5ada140dec9631c4d130caa488a4b2e
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-09-26 22:37:54 +0300

    prepare for deployment

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,6 @@
 from app import create_app
-from app.config import ProductionConfig
 
-app = create_app(config_class=ProductionConfig)
+app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
